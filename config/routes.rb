fab5497e11bd79de52b85b8fc5d996334540e280
Raghu Betina <raghu@firstdraft.com>
2019-06-12 03:53:59 +0000

    Generate cuisines resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the Cuisine resource:
+
+  # CREATE
+  get("/cuisines/new", { :controller => "cuisines", :action => "new_form" })
+  post("/create_cuisine", { :controller => "cuisines", :action => "create_row" })
+
+  # READ
+  get("/cuisines", { :controller => "cuisines", :action => "index" })
+  get("/cuisines/:id_to_display", { :controller => "cuisines", :action => "show" })
+
+  # UPDATE
+  get("/cuisines/:prefill_with_id/edit", { :controller => "cuisines", :action => "edit_form" })
+  post("/update_cuisine/:id_to_modify", { :controller => "cuisines", :action => "update_row" })
+
+  # DELETE
+  get("/delete_cuisine/:id_to_remove", { :controller => "cuisines", :action => "destroy_row" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
